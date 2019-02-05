c45893481858d8c0f8c8989949cd1bdfad52b9e9
Chris Hundhausen <chundhau@gmail.com>
2019-02-04 23:02:31 -0800

    Initial commit: Start point for 2/5/19 class

--- js/doGet.js
+++ js/doGet.js
@@ -1,21 +1,17 @@
 //********************************************************************************
 //DOGET.GS
 //*********************************************************************************
-//This file contains the doGet() function, which serves up the web-based Play 
+//This file contains the doGet() function, which serves up the web-based  
 //app, responding to get requests with query strings as appropriate to dish out
 //new pages
 //*********************************************************************************
 
-function doGet() {
-  var faviconUrl = "https://dl.dropboxusercontent.com/s/79q9a1xy2148unf/SpeedScore3Icon.ico";
-  var template = HtmlService.createTemplateFromFile('html/app');
-  return template
-        .evaluate()
-        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
-        .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-        .setFaviconUrl(faviconUrl)
-        .setTitle("SpeedScore Track")
-        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);     
+function doGet(e) {
+  return HtmlService.createTemplateFromFile('html/app')
+    .evaluate()
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setTitle("Test Speedgolf Web Service");
 }
 
 //include: Allows us to include files using templated HTML, per Google's best practices 
@@ -23,4 +19,4 @@ function doGet() {
 function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename)
       .getContent();
-}
+}   
